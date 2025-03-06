24a19f4609a7d03174a14aefa46283a0c1d6dc28
Sakar Giri <sakargiri365@gmail.com>
2025-03-06 22:22:03 +0545

    wifi-config

--- status/wifi.sh
+++ status/wifi.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Function to get the current Wi-Fi signal strength
-get_signal_strength() {
-    local signal
-    signal=$(nmcli -g IN-USE,SIGNAL dev wifi | grep '*' | awk -F: '{print $2}')
-    echo "Wi-Fi: ${signal}%"
-}
-
-# Function to show Wi-Fi QR code (when hovered or clicked)
-show_qr_code() {
-    nmcli device wifi show
-}
-
-# Check argument for hover/click
-if [[ $1 == "qr" ]]; then
-    show_qr_code
-else
-    get_signal_strength
-fi
+local signal
+signal=$(nmcli -g IN-USE,SIGNAL dev wifi | grep '*' | awk -F: '{print $2}')
+echo "Wi-Fi: ${signal}%"
