086a261fe3c689a1f4495828aa2b96ebbc996b41
Sakar Giri <sakargiri365@gmail.com>
2025-03-06 16:40:57 +0545

    dmenu

--- dwm/config.h
+++ dwm/config.h
@@ -71,7 +71,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
