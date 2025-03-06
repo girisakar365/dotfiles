1bd985f4bd419d308194d1de86e19c83206995ce
Sakar Giri <sakargiri365@gmail.com>
2025-03-06 13:46:39 +0545

    color-tweaks

--- dmenu/config.h
+++ dmenu/config.h
@@ -2,9 +2,9 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xff;     /* Amount of opacity. 0xff is opaque             */
+static const unsigned int alpha = 0xcf;     /* Amount of opacity. 0xff is opaque             */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 800;                    /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 static const int user_bh = 5;              /* -h option; minimum height of a menu line */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -14,8 +14,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	// [SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeNorm] = { "#EEEEEE", "#030303" },
+	// [SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#FFFFFF", "#1E1E1E" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
