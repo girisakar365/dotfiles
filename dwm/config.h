cd25c80bd3ce9e0420a845da5db86033e24a29c1
Sakar Giri <sakargiri365@gmail.com>
2025-03-07 15:37:01 +0545

    volume-config

--- dwm/config.h
+++ dwm/config.h
@@ -108,8 +108,8 @@ static const Key keys[] = {
     
 	{ 0, XF86XK_MonBrightnessUp,   spawn,  SHCMD("brightnessctl set +10%") },
     { 0, XF86XK_MonBrightnessDown, spawn,  SHCMD("brightnessctl set 10%-") },
-	{ 0, XF86XK_AudioRaiseVolume, spawn, SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+") },
-    { 0, XF86XK_AudioLowerVolume, spawn, SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-") },
+	{ 0, XF86XK_AudioRaiseVolume, spawn, SHCMD("volumeup") },
+    { 0, XF86XK_AudioLowerVolume, spawn, SHCMD("volumedown") },
     { 0, XF86XK_AudioMute, spawn, SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
