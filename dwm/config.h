c74de0e71b730805779f17ff6fef0673ed1a98e0
Sakar Giri <sakargiri365@gmail.com>
2025-03-07 16:22:27 +0545

    statusbar-conf

--- dwm/config.h
+++ dwm/config.h
@@ -17,7 +17,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "monospace:size=11", "Noto Color Emoji:size=11", "Font Awesome 6 Free:size=11"};
+static const char *fonts[]          = { "monospace:size=11", "Noto Color Emoji:size=11", "Font Awesome 6 Free:size=12"};
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -32,7 +32,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -52,9 +52,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[M]",      monocle }, /* first entry is default */
+	{ "",      monocle }, /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[]=",      tile },    
+	{ "  ",      tile },    
 };
 
 /* key definitions */
