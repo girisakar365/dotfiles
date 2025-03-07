fd62b3a989723c104c8174f46a467e4bb6c99ce8
Sakar Giri <sakargiri365@gmail.com>
2025-03-07 19:13:17 +0545

    key-bindings-conf

--- dwm/config.h
+++ dwm/config.h
@@ -41,7 +41,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -53,14 +53,16 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "",      monocle }, /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "  ",      tile },    
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
 #define MODKEY Mod4Mask
+#define ALTKEY Mod1Mask
+
 #define TAGKEYS(KEY,TAG) \
-	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ ALTKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
@@ -75,42 +77,45 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY, 						XK_b, 	   spawn,	   SHCMD("firefox")}, 
+
+	// spwan
+	{ MODKEY, 						XK_1, 	   spawn,	   SHCMD("st -e ranger")}, 
+	{ MODKEY, 						XK_2, 	   spawn,	   SHCMD("firefox")}, 
+	{ MODKEY, 						XK_3, 	   spawn,	   SHCMD("code")}, 
+	{ MODKEY|ShiftMask, 			XK_s, 	   spawn,	   {.v = dmenucmd} }, 
+	{ MODKEY,                       XK_s,  spawn,      SHCMD("flameshot gui") },
+	{ MODKEY,            			XK_Return, spawn,      {.v = termcmd } },
 	{ MODKEY, 						XK_e, 	   spawn,	   SHCMD("st -e ranger")}, 
+	{ MODKEY, 						XK_b, 	   spawn,	   SHCMD("firefox")}, 
 	{ MODKEY, 						XK_c, 	   spawn,	   SHCMD("code")}, 
-	{ MODKEY|ShiftMask, 			XK_s, 	   spawn,	   SHCMD("flameshot gui")}, 
 	
-	{ MODKEY,                       XK_space,  spawn,      {.v = dmenucmd } },
-	{ MODKEY,            XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+
+	{ MODKEY,                       XK_j,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
-	{ MODKEY|ShiftMask,				XK_Tab,    view,           {0} },
+	{ MODKEY,						XK_Tab,    view,           {0} },
 	{ MODKEY,             			XK_q,      killclient,     {0} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_p,  	   setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+
+	// mode shift
+	{ MODKEY,                    	XK_m,      setlayout,      {.v = &layouts[0]} }, // M
+	{ MODKEY,                    	XK_t,      setlayout,      {.v = &layouts[1]} }, // T
+	{ MODKEY,                    	XK_f,      setlayout,      {.v = &layouts[2]} }, // FF
+	{ MODKEY,                       XK_space,  	   setlayout,      {0} },
 	
-	{ Mod1Mask,						XK_Tab,    altTabStart,           {0} },
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	
+	{ ALTKEY,						XK_Tab,    altTabStart,           {0} },
     
+	// brightness
 	{ 0, XF86XK_MonBrightnessUp,   spawn,  SHCMD("brightnessctl set +10%") },
     { 0, XF86XK_MonBrightnessDown, spawn,  SHCMD("brightnessctl set 10%-") },
+
+	// volume
 	{ 0, XF86XK_AudioRaiseVolume, spawn, SHCMD("volumeup") },
     { 0, XF86XK_AudioLowerVolume, spawn, SHCMD("volumedown") },
     { 0, XF86XK_AudioMute, spawn, SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle") },
+
+	// tags
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -120,7 +125,23 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+	// kill dwm
+	{ MODKEY|ShiftMask,             XK_k,      quit,           {0} },
+
+	//not used
+	// { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	// { MODKEY,                       XK_l,      focusstack,     {.i = -1 } },
+	// { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	// { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	// { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+	// { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	// { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	// { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	// { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	// { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	// { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	
 };
 
 /* button definitions */
@@ -129,11 +150,10 @@ static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
-	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
