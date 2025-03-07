c3d0d30f6e9fc51de97ca3f547d7eb2e29672b28
Sakar Giri <sakargiri365@gmail.com>
2025-03-07 10:51:15 +0545

    awsome-font

--- dwm/config.h
+++ dwm/config.h
@@ -17,7 +17,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "monospace:size=11", "Noto Color Emoji:size=11" };
+static const char *fonts[]          = { "monospace:size=11", "Noto Color Emoji:size=11", "Font Awesome 6 Free:size=11"};
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
