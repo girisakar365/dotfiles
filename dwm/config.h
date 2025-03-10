01bbd3431682d0d3c42f038bb879a2c3b0fe94dd
Sakar Giri <sakargiri365@gmail.com>
2025-03-10 20:28:19 +0545

    key-bindings

--- dwm/config.h
+++ dwm/config.h
@@ -4,10 +4,7 @@
 /* alt-tab configuration */
 static const unsigned int tabModKey 		= 0x40;	/* if this key is hold the alt-tab functionality stays acitve. This key must be the same as key that is used to active functin altTabStart `*/
 static const unsigned int tabCycleKey 		= 0x17;	/* if this key is hit the alt-tab program moves one position forward in clients stack. This key must be the same as key that is used to active functin altTabStart */
-static const unsigned int tabPosY 			= 1;	/* tab position on Y axis, 0 = bottom, 1 = center, 2 = top */
-static const unsigned int tabPosX 			= 1;	/* tab position on X axis, 0 = left, 1 = center, 2 = right */
-static const unsigned int maxWTab 			= 600;	/* tab menu width */
-static const unsigned int maxHTab 			= 100;	/* tab menu height */
+
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
@@ -62,7 +59,7 @@ static const Layout layouts[] = {
 #define ALTKEY Mod1Mask
 
 #define TAGKEYS(KEY,TAG) \
-	{ ALTKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
@@ -79,15 +76,17 @@ static const Key keys[] = {
 	/* modifier                     key        function        argument */
 
 	// spwan
-	{ MODKEY, 						XK_1, 	   spawn,	   SHCMD("st -e ranger")}, 
-	{ MODKEY, 						XK_2, 	   spawn,	   SHCMD("firefox")}, 
-	{ MODKEY, 						XK_3, 	   spawn,	   SHCMD("code")}, 
-	{ MODKEY|ShiftMask, 			XK_s, 	   spawn,	   {.v = dmenucmd} }, 
-	{ MODKEY,                       XK_s,  	   spawn,      SHCMD("flameshot gui") },
+	{ ALTKEY, 						XK_1, 	   spawn,	   SHCMD("st -e ranger")}, 
+	{ ALTKEY, 						XK_2, 	   spawn,	   SHCMD("firefox")}, 
+	{ ALTKEY, 						XK_3, 	   spawn,	   SHCMD("code")}, 
+	{ MODKEY, 						XK_s, 	   spawn,	   {.v = dmenucmd} }, 
+	{ MODKEY|ShiftMask,				XK_s,  	   spawn,      SHCMD("flameshot gui") },
 	{ MODKEY,            			XK_Return, spawn,      {.v = termcmd } },
 	{ MODKEY, 						XK_e, 	   spawn,	   SHCMD("st -e ranger")}, 
 	{ MODKEY, 						XK_b, 	   spawn,	   SHCMD("firefox")}, 
-	{ MODKEY, 						XK_c, 	   spawn,	   SHCMD("code")}, 
+	{ MODKEY, 						XK_c, 	   spawn,	   SHCMD("code")},
+	{ MODKEY|ShiftMask, 			XK_m, 	   spawn,	   SHCMD("st -e btop")},
+
 	
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 
