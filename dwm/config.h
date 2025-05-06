0da53a39e414c3d1597b875cf5ed02cfa220fca6
Sakar Giri <sakargiri365@gmail.com>
2025-05-06 14:09:39 +0545

    tweaks

--- dwm/config.h
+++ dwm/config.h
@@ -7,9 +7,9 @@ static const unsigned int tabCycleKey 		= 0x17;	/* if this key is hit the alt-ta
 
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 10;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 0;       /* vertical padding of bar */
@@ -117,8 +117,7 @@ static const Key keys[] = {
     { 0, XF86XK_AudioMute, spawn, SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle") },
 
 	// sleep / lock
-	{ MODKEY,                       XK_l,      spawn,           SHCMD("lock") },
-
+	{ MODKEY,                       XK_l,      spawn,           SHCMD("slock lock") },
 
 	// tags
 	TAGKEYS(                        XK_1,                      0)
