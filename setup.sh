35e4549c1330e28c5c8e27ef074304b28ec1782a
Sakar Giri <sakargiri365@gmail.com>
2025-05-16 13:11:00 +0545

    setup-script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 echo Installing default pkgs:
-sudo pacman -S --needed ttf-dejavu ttf-noto-nerd libxft libxinerama feh xdotool wmctrl
+sudo pacman -S --needed ttf-dejavu ttf-noto-nerd libxft libxinerama feh xdotool wmctrl brightnessctl
+sudo pacman --sync flameshot
+sudo pacman -S firefox git unzip wget
+
+echo Installing yay:
+mkdir -p ~/Downloads
+cd ~/Downloads
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ../..
+yay -S visual-studio-code-bin
+yay -S nerd-fonts-jetbrains-mono
+yay -S nerd-fonts-hack
+yay -S nerd-fonts-ubuntu-mono
